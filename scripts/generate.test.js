55ba0f8a077d7e957ce688a68a6e04dff096db85
SnO2WMaN <sno2wman.official@gmail.com>
2020-01-01 17:09:23 +0900

    📷 Update test snapshot

--- scripts/generate.test.js
+++ scripts/generate.test.js
@@ -6,7 +6,7 @@ test("emoji-cheat-sheet", async () => {
   expect(await generate()).toMatchInlineSnapshot(`
     "# emoji-cheat-sheet
 
-    [![build](https://travis-ci.org/ikatyang/emoji-cheat-sheet.svg?branch=master)](https://travis-ci.org/ikatyang/emoji-cheat-sheet)
+    [![Actions Status](https://github.com/SnO2WMaN/emoji-cheat-sheet/workflows/Node%20CI/badge.svg)](https://github.com/SnO2WMaN/emoji-cheat-sheet/actions)
 
     This cheat sheet is automatically generated from [GitHub Emoji API](https://api.github.com/emojis) and [Unicode Full Emoji List](https://unicode.org/emoji/charts/full-emoji-list.html).
 
